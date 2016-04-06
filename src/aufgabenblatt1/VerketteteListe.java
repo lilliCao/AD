1fd043a42532e6fc003e011258aa38ff7064c74e
unknown <caothivananh98@gmail.com>
2016-04-06 10:04:24 +0600

    aufgabenblatt 1 edited

--- src/aufgabenblatt1/VerketteteListe.java
+++ src/aufgabenblatt1/VerketteteListe.java
@@ -1,20 +1,20 @@
 package aufgabenblatt1;
 
+import java.io.ObjectInputStream.GetField;
 import java.util.LinkedList;
 
 public class VerketteteListe<T> implements Liste<T> {
 	int anzahlDerElemnet;
-	LinkedList list;
-	T naechstesElement;
+	LinkedList<T> list;
 	T stopElement;
 
 	/**
+	 * 
 	 * Konstruktor
 	 */
 	public VerketteteListe() {
 		anzahlDerElemnet = 0;
-		list = new LinkedList();
-		naechstesElement = null;
+		list = new LinkedList<T>();
 	}
 
 	/**
@@ -24,55 +24,78 @@ public class VerketteteListe<T> implements Liste<T> {
 		return anzahlDerElemnet;
 	}
 
-	public LinkedList getList() {
+	public LinkedList<T> getList() {
 		return list;
-	}
-
-	public T getNaechstesElement() {
-		return naechstesElement;
 	}
 
 	public T getStopElement() {
 		return stopElement;
 	}
 
-	public void setNaechstesElement(T naechstesElement) {
-		this.naechstesElement = naechstesElement;
+	public void setStopElement(T stopElement) {
+		this.stopElement = stopElement;
 	}
 
 	@Override
-	public void insert(int pos, Object elem) {
-		// TODO Auto-generated method stub
+	public void insert(int pos, Object elem) throws IndexOutOfBoundsException {
+		//list.add(pos, (T) elem);
 
+		
+		
+		
+		
 	}
 
 	@Override
-	public void delete(int pos) {
-		// TODO Auto-generated method stub
-
+	public void delete(int pos) throws IndexOutOfBoundsException {
+		if (list.get(pos) != null) {
+			anzahlDerElemnet--;
+		}
+		list.set(pos, null);
 	}
 
 	@Override
 	public int find(Object elem) {
-		// TODO Auto-generated method stub
-		return 0;
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).equals(elem)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	@Override
-	public T retrieve(int pos) {
-		// TODO Auto-generated method stub
-		return null;
+	public T retrieve(int pos) throws IndexOutOfBoundsException {
+		return (T) list.get(pos);
+
 	}
 
 	@Override
 	public void concat(Liste otherlist) {
 		VerketteteListe<T> otherlistV = (VerketteteListe<T>) otherlist;
-		setNaechstesElement(otherlistV.stopElement);
+		setStopElement((T) otherlistV.list.getFirst());
 	}
 
 	@Override
 	public int size() {
 		return anzahlDerElemnet;
 	}
+public static void main(String[] args) {
+	 VerketteteListe<Integer> test = new VerketteteListe<Integer>();
+test.insert(0, 0);
+test.insert(1, 10);
+test.insert(2, 11);
+System.out.println(test.list.get(0));
+System.out.println(test.list.get(1));
+System.out.println(test.list.get(2));
+System.out.println("!!!");
+test.delete(1);
+//test.insert(1, 1);
+System.out.println(test.list.get(0));
+System.out.println(test.list.get(1));
+System.out.println(test.list.get(2));
 
+
+
+}
 }
